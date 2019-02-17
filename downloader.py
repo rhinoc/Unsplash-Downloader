70c37d1c5e1d1eec27a8928269f952319128b9d3
rhinoc <rhinoc@outlook.com>
2019-02-17 10:25:46 +0800

    updated to a multi-thread downloader

--- downloader.py
+++ downloader.py
@@ -1,6 +1,6 @@
 #! python3
 
-import os, bs4, requests, re, time
+import os, bs4, requests, re, time, threading
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
@@ -9,7 +9,7 @@ def scroll_down(driver, times):
     for i in range(times):
         print('.',end='')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)
+        time.sleep(5)
     print('')
 
 os.makedirs('pics',exist_ok=True)
@@ -28,40 +28,52 @@ print('Driver prepared')
 scroll_down(driver,3)
 soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
 link = soup.find_all('a',{'class':'_2Mc8_'})
-amount = str(soup.find('p',{'class':'_1u88E _1iWCF _27Bp2'}))
+amount = len(link)
+total = str(soup.find('p',{'class':'_1u88E _1iWCF _27Bp2'}))
 rr = re.compile(r'.*>(.*?)free.*')
-amount = rr.findall(amount)[0].strip()
-amount = int(amount.replace(',',''))
-print('Total: '+ str(amount))
-print('Captured: '+ str(len(link)))
+total = rr.findall(total)[0].strip()
+total = int(total.replace(',',''))
+print('Total: '+ str(total))
+print('Captured: '+ str(amount))
 
-count = 0
-for piece in link:
-    deepLk = 'https://unsplash.com' + str(piece.get('href'))
-    headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Origin': 'https://unsplash.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    deepRes = requests.get((deepLk), headers=headers)
-    deepSoup = bs4.BeautifulSoup(deepRes.text, "html.parser")
-    pic = deepSoup.find('img',{'class':'_2zEKz'})
-    try:
-        picLk = str(pic.get('src'))
-    except:
-        print('download failed')
-    rr = re.compile(r'(.* ?)\?')
-    filename = str(pic.get('alt')) + '.png'
-    if picLk != 'None':
-        count += 1
-        picLk = rr.findall(picLk)[0]
-        print('Download from ' + deepLk + ': ' + filename )
-        picRes = requests.get(picLk)
-        if filename == 'None.png':
-            imageFile = open(os.path.join('pics', os.path.basename(picLk)+'.png'),'wb')
-        else:
-            imageFile = open(os.path.join('pics', filename),'wb')
-        for chunk in picRes.iter_content(100000):
+
+def singleDownloader(startNum, endNum):
+    for piece in link[startNum:endNum]:
+        deepLk = 'https://unsplash.com' + str(piece.get('href'))
+        headers = {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Origin': 'https://unsplash.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        deepRes = requests.get((deepLk), headers=headers)
+        deepSoup = bs4.BeautifulSoup(deepRes.text, "html.parser")
+        pic = deepSoup.find('img', {'class': '_2zEKz'})
+        try:
+            picLk = str(pic.get('src'))
+            rr = re.compile(r'(.* ?)\?')
+            filename = str(pic.get('alt')) + '.png'
+            picLk = rr.findall(picLk)[0]
+            print('Downloading from ' + deepLk + ': ' + filename)
+            picRes = requests.get(picLk)
+            if filename == 'None.png':
+                imageFile = open(os.path.join('pics', os.path.basename(picLk) + '.png'), 'wb')
+            else:
+                imageFile = open(os.path.join('pics', filename), 'wb')
+            for chunk in picRes.iter_content(100000):
                 imageFile.write(chunk)
-        imageFile.close()
+            imageFile.close()
+        except:
+            print('Download failed: Can not get the download link.')
+
+downloadThreads = []
+delta = round(amount/8)
+for i in range(0, amount, delta):
+    endNum = min(amount - 1, i + delta - 1)
+    downloadThread = threading.Thread(target=singleDownloader, args=(i, endNum))
+    downloadThreads.append(downloadThread)
+    downloadThread.start()
+
+for downloadThread in downloadThreads:
+    downloadThread.join()
+print('Download Finished.')
